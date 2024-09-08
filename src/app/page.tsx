207e12bf8055bccc064815723348babf959e32e2
GustavoCR021 <gustavoc1922@gmail.com>
2024-09-07 23:42:38 -0300

    feat: test header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-
+import Header from "./components/Header"
 
 export default function Home() {
   return (
-    <div>
-      <p>
-        test
-      </p>
-    </div>
+    <>
+      <Header />
+      <div>
+        <p>
+          test
+        </p>
+      </div>
+    </>
   );
 }
