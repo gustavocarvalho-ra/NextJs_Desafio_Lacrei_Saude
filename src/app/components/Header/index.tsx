192e452eec91344675b712aaa0da41687997806f
GustavoCR021 <gustavoc1922@gmail.com>
2024-09-09 18:29:33 -0300

    feat: add image logo and test display and root color

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -5,7 +5,13 @@ import { MainHeader } from "./styles";
 export default function Header() {
   return (
     <MainHeader>
-      <h1>Lacrei Saúde</h1>
+      <h1><span>LS </span>Lacrei Saúde</h1>
+
+      <div className="btn">
+        <button>Sobre</button>
+        <button>Login</button>
+        <button>Ajuda</button>
+      </div>
 
     </MainHeader>
   );
--- src/app/components/Header/styles.ts
+++ src/app/components/Header/styles.ts
@@ -3,8 +3,15 @@ import styled from "styled-components"
 export const MainHeader = styled.div `
   width: 100%;
   height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 
   h1 {
-    color: {--primary-color};
+    color: var(--primary-color);
+  
+    span {
+      font-weight: 800;
+    }
   }
 `
