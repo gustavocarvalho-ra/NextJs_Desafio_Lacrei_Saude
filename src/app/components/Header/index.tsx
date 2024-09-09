3b28bce75c7d8e1fe40136e3fb314ff507ebbf71
GustavoCR021 <gustavoc1922@gmail.com>
2024-09-09 19:32:58 -0300

    feat: add font "Nunito"

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+// import Image from "next/image";
 import { MainHeader } from "./styles";
 
 export default function Header() {
   return (
     <MainHeader>
+      {/* <Image src={"../../../public/logo.png"} alt="Logo Lacrei Saúde" width={100} height={18}></Image> */}
+      {/* <Image src={"/src/public/logo.png"} alt="Logo Lacrei Saúde" width={1000} height={180}></Image> */}
       <h1><span>LS </span>Lacrei Saúde</h1>
 
       <div className="btn">
