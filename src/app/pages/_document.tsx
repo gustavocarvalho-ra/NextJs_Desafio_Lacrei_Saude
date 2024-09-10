a56c2a827d3c9f1d7e55f5fcd2b3daabb86a32a4
GustavoCR021 <gustavoc1922@gmail.com>
2024-09-10 11:41:52 -0300

    feat: add _document

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -10,8 +10,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: React.ComponentType) => (props: React.ComponentProps<typeof App>) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
